f24efed7bc3b0a0b51710bbd933e56903759e7b3
simonduck123 <la19039906@gmail.com>
2024-05-13 17:40:38 +0200

    monday

--- QuickPrototype2/Assets/Scripts/ScreenShake.cs
+++ QuickPrototype2/Assets/Scripts/ScreenShake.cs
@@ -6,15 +6,18 @@ using Cinemachine;
 public class ScreenShake : MonoBehaviour
 {
     private CinemachineVirtualCamera cinemachineVirtualCamera;
-    public float ShakeIntensity = 1f;
-    public float ShakeTime = 0.2f;
-    private float timer;
-
     private CinemachineBasicMultiChannelPerlin _cbmcp;
+    public AnimationCurve shakeIntensityCurve; // Serialized field for animation curve
+    public float ShakeTime = 0.2f;
+    public float strength = 1.0f;
+    private float timer;
+    public AudioClip shakeSound;
+    public AudioSource audioSource;
 
     private void Awake()
     {
         cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+        _cbmcp = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
     }
 
     private void Start()
@@ -22,36 +25,50 @@ public class ScreenShake : MonoBehaviour
         StopShake();
     }
 
+    // Method to shake the camera with the specified curve
     public void ShakeCamera()
     {
-        CinemachineBasicMultiChannelPerlin _cbmcp = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-        _cbmcp.m_AmplitudeGain = ShakeIntensity;
-
         timer = ShakeTime;
+        PlayAudio(shakeSound);
     }
 
     void StopShake()
     {
-        CinemachineBasicMultiChannelPerlin _cbmcp = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-        _cbmcp.m_AmplitudeGain = 0f;
-        timer = 0;
+        if (_cbmcp != null)
+        {
+            _cbmcp.m_AmplitudeGain = 0f;
+            timer = 0;
+        }
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.F1))
+        if (Input.GetKeyDown(KeyCode.F1))
         {
             ShakeCamera();
         }
 
-        if(timer > 0)
+        if (timer > 0)
         {
+            
+            float curveValue = shakeIntensityCurve.Evaluate(1 - (timer / ShakeTime)); // Evaluate curve based on time
+            curveValue = curveValue * strength;
+            if (_cbmcp != null)
+            {
+                _cbmcp.m_AmplitudeGain = curveValue;
+            }
+
             timer -= Time.deltaTime;
 
-            if(timer <= 0)
+            if (timer <= 0)
             {
                 StopShake();
             }
         }
+    }
+    private void PlayAudio(AudioClip audio)
+    {
+        audioSource.clip = audio;
+        audioSource.Play();
     }
 }

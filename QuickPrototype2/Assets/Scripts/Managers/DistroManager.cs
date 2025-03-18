d5ac31dd1b0cbe247857053250b054ed500fd63a
Simon Duckham <la19039906@gmail.com>
2025-03-18 16:53:51 +0100

    fps counter

--- QuickPrototype2/Assets/Scripts/Managers/DistroManager.cs
+++ QuickPrototype2/Assets/Scripts/Managers/DistroManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 public class DistroManager : MonoBehaviour
 {
     private Dictionary<string, System.Action> actionMap;
-    public DistroTransController distroTransController;
+    //public DistroTransController distroTransController;
 
     void Start()
     {
         actionMap = new Dictionary<string, System.Action>();
         //transition
-        actionMap.Add("/distro/transition/do", distroTransController.DoTransition);
+        //actionMap.Add("/distro/transition/do", distroTransController.DoTransition);
     }
 
     public void HandleAction(string address)
